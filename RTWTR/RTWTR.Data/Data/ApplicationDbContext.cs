1f9d41fbd3bd2900dc01a6847ac256235b1e49c4
Nikolay Nikolov <nikolaytsnikolov@gmail.com>
2018-04-13 18:46:16 +0300

    Skeleton

--- RTWTR/RTWTR.Data/Data/ApplicationDbContext.cs
+++ RTWTR/RTWTR.Data/Data/ApplicationDbContext.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using RTWTR.MVC.Models;
+using RTWTR.Data.Models;
 
 namespace RTWTR.MVC.Data
 {
