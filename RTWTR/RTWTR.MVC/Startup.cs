e69bb2c692a9541f759e3f86e1c9c84b17f3fbd0
IovoslavIovchev <iovoslav.i@gmail.com>
2018-04-17 12:36:44 +0300

    Added Administration Area

--- RTWTR/RTWTR.MVC/Startup.cs
+++ RTWTR/RTWTR.MVC/Startup.cs
@@ -59,6 +59,11 @@ namespace RTWTR.MVC
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+                    name: "areas",
+                    template: "{area:exists}/{controller=Home}/{action=Index}"
+                );
+
+                routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
