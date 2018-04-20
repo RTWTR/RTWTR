f47ad6e26b529194936902679653bc06a5c6bca8
Nikolay Nikolov <nikolaytsnikolov@gmail.com>
2018-04-20 16:12:46 +0300

    Fixing automapper

--- RTWTR/RTWTR.DTO/CollectionDTO.cs
+++ RTWTR/RTWTR.DTO/CollectionDTO.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using AutoMapper;
-using RTWTR.Data.Models;
-using RTWTR.Infrastructure.Mapping;
-using StackExchange.Redis;
+﻿using System.Collections.Generic;
 
 namespace RTWTR.DTO
 {
-    public class CollectionDTO : IMapFrom<Collection>
+    public class CollectionDTO
     {
         public string Name { get; set; }
 
