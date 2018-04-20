74f197856c619e3f7a7340f1f2d8324cbe56d761
Nikolay Nikolov <nikolaytsnikolov@gmail.com>
2018-04-20 16:47:08 +0300

    Implementing interface

--- RTWTR/RTWTR.Service.Data/CollectionService.cs
+++ RTWTR/RTWTR.Service.Data/CollectionService.cs
@@ -1,10 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using RTWTR.DTO;
+using RTWTR.Service.Data.Contracts;
 
 namespace RTWTR.Service.Data
 {
-    public class CollectionService
+    public class CollectionService :ICollectionService
     {
+        public ICollection<CollectionDTO> GetUserCollections(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddTweetToCollection(string userId, string collectionName, string tweetId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveTweetFromCollection(string userId, string collectionName, string tweetId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CollectionDTO ShowUserCollection(string userId, string collectionName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CollectionDTO GetUserCollectionByName(string collectionName, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveCollection(string userId, string collectionName)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
