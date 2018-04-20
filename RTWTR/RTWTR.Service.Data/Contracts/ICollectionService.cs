259fb012b91519e3b45ec0a7685fefb7754b300e
Nikolay Nikolov <nikolaytsnikolov@gmail.com>
2018-04-20 16:38:24 +0300

    Adding DTOs to ICollectionService

--- RTWTR/RTWTR.Service.Data/Contracts/ICollectionService.cs
+++ RTWTR/RTWTR.Service.Data/Contracts/ICollectionService.cs
@@ -1,21 +1,20 @@
-﻿using System;
+﻿using RTWTR.DTO;
 using System.Collections.Generic;
-using System.Text;
 
 namespace RTWTR.Service.Data.Contracts
 {
     public interface ICollectionService
     {
         //Should be DTO not void
-        void GetUserCollections(string userId);
+        ICollection<CollectionDTO> GetUserCollections(string userId);
              
         void AddTweetToCollection(string userId,string collectionName, string tweetId);
 
         void RemoveTweetFromCollection(string userId,string collectionName, string tweetId);
 
-        void ShowUserCollection(string userId, string collectionName);
+        CollectionDTO ShowUserCollection(string userId, string collectionName);
 
-        void GetUserCollectionByName(string collectionName, string userId);
+        CollectionDTO GetUserCollectionByName(string collectionName, string userId);
 
         void RemoveCollection(string userId, string collectionName);
 
