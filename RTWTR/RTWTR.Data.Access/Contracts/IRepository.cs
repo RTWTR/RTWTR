f3643d1760cfe9be017347f4efa2c9b65efd3f38
Nikolay Nikolov <nikolaytsnikolov@gmail.com>
2018-04-20 18:05:48 +0300

    Implementing Saver

--- RTWTR/RTWTR.Data.Access/Contracts/IRepository.cs
+++ RTWTR/RTWTR.Data.Access/Contracts/IRepository.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
 
 namespace RTWTR.Data.Access.Contracts
 {
-    interface IRepository<T> 
+    public interface IRepository<T> 
     {
         T GetById(string id);
 
