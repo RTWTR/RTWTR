f178bc577a697d822017032f0e49888508cffdd3
IovoslavIovchev <iovoslav.i@gmail.com>
2018-04-20 17:09:06 +0300

    Added a new TwitterApiProvider & TokenEncoder

--- RTWTR/RTWTR.Service.Twitter/TwitterApiProvider.cs
+++ RTWTR/RTWTR.Service.Twitter/TwitterApiProvider.cs
@@ -1,9 +1,10 @@
 using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
 using System.IO;
 using System.Net;
-using System.Web;
+using System.Net.Http;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json.Linq;
 using RTWTR.Infrastructure.Contracts;
 using RTWTR.Service.Twitter.Contracts;
 
@@ -11,94 +12,94 @@ namespace RTWTR.Service.Twitter
 {
     public class TwitterApiProvider : IApiProvider
     {
-        private readonly IHeaderGenerator headerGenerator;
+        private readonly IEncoder encoder;
 
-        public TwitterApiProvider(IHeaderGenerator headerGenerator)
+        private string bearerToken;
+
+        private readonly IVariableProvider variableProvider;
+
+        public TwitterApiProvider(IEncoder encoder, IVariableProvider variableProvider)
         {
-            this.headerGenerator = headerGenerator ?? throw new ArgumentNullException(nameof(headerGenerator));
+            this.variableProvider = variableProvider ??
+                throw new ArgumentNullException(nameof(variableProvider));
+            this.encoder = encoder ??
+                throw new ArgumentNullException(nameof(encoder));
         }
 
         public string GetJSON(string url)
         {
-            List<string> parameters;
-            if (url.Contains("?"))
+            if (this.bearerToken == null)
             {
-                parameters = GetQueryParameters(url);
-                url = url.Substring(0, url.IndexOf('?'));
-            }
-            else 
-            {
-                parameters = null;
+                SetBearerToken();
             }
 
-            string header = this.headerGenerator.GenerateHeader(url, parameters);
-
-            WebResponse response = SendRequest(url, header, parameters);
-
-            string responseData = string.Empty;
-
-            using (StreamReader stream = new StreamReader(response.GetResponseStream()))
-            {
-                responseData = stream.ReadToEnd();
-            }
-
-            return responseData;
-        }
-
-        private List<string> GetQueryParameters(string url) 
-        {
-            List<string> result = new List<string>();
-
-            string queryString = url.Substring(url.IndexOf('?') + 1);
-            NameValueCollection nameValueCollection = HttpUtility.ParseQueryString(queryString);
-
-            foreach (string param in nameValueCollection)
-            {
-                result.Add(
-                    string.Concat(
-                        param, 
-                        "=",
-                        Uri.EscapeDataString(nameValueCollection[param])
-                    )
-                );
-            }
-
-            return result;
-        }
-
-        private string GetRequestBody(List<string> parameters)
-        {
-            return string.Join("&", parameters);
-        }
-
-        private WebResponse SendRequest(string url, string header, List<string> parameters)
-        {
-            ServicePointManager.Expect100Continue = false;
-            string requestBody = string.Empty;
-
-            if (parameters != null)
-            {
-                requestBody = GetRequestBody(parameters);
-            }
-
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(
-                string.Concat(
-                    url,
-                    "?",
-                    requestBody
-                )
+            WebRequest request = GetRequest(
+                url, 
+                "GET", 
+                $"Bearer {this.bearerToken}", 
+                "application/x-www-form-urlencoded;charset=UTF-8"
             );
 
-            // Add the generated OAuth header
-            request.Headers.Add("Authorization", header);
+            return this.GetResponse(request);
+        }
 
-            // Set the request method
-            request.Method = "GET";
+        private void SetBearerToken()
+        {
+            string consumerKey = this.variableProvider.GetValue("rtwtr_consumerKey");
+            string consumerSecret = this.variableProvider.GetValue("rtwtr_consumerSecret");
 
-            // Set the content type
-            request.ContentType = "application/x-www-form-urlencoded";
+            string bearerToken = this.encoder.Encode(consumerKey, consumerSecret);
 
-            return request.GetResponse();
+            WebRequest request = this.GetRequest(
+                "https://api.twitter.com/oauth2/token",
+                "POST",
+                $"Basic {bearerToken}",
+                "application/x-www-form-urlencoded;charset=UTF-8",
+                "grant_type=client_credentials"
+            );
+
+            string jsonResponse = GetResponse(request);
+
+            JObject parsedResponse = JObject.Parse(jsonResponse);
+
+            this.bearerToken = parsedResponse["access_token"].ToString();
+        }
+
+        private WebRequest GetRequest(string url, string method, string authorization, string contentType, string body = null)
+        {
+            WebRequest request = WebRequest.Create(url);
+            request.Method = method;
+            request.Headers.Add("Authorization", authorization);
+            request.ContentType = contentType;
+
+            if (body != null)
+            {
+                byte[] requestBody = Encoding.UTF8.GetBytes(body);
+
+                using(Stream stream = request.GetRequestStream())
+                {
+                    stream.Write(requestBody, 0, requestBody.Length);
+                }
+            }
+
+            return request;
+        }
+
+        private string GetResponse(WebRequest request)
+        {
+            HttpWebResponse response = (HttpWebResponse) request.GetResponse();
+
+            string jsonResponse = string.Empty;
+
+            if (response.StatusCode.Equals(HttpStatusCode.OK))
+            {
+                using(StreamReader stream = new StreamReader(response.GetResponseStream()))
+                {
+                    jsonResponse = stream.ReadToEnd();
+                }
+            }
+
+            return jsonResponse;
         }
     }
 }
