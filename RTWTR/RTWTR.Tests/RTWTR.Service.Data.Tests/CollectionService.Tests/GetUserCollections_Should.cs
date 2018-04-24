67fd5cd39a2a4efb65c805947ea2a8176e06e247
IovoslavIovchev <iovoslav.i@gmail.com>
2018-04-24 19:49:59 +0300

    Added tests for GetUserCollections_Should

--- RTWTR/RTWTR.Tests/RTWTR.Service.Data.Tests/CollectionService.Tests/GetUserCollections_Should.cs
+++ RTWTR/RTWTR.Tests/RTWTR.Service.Data.Tests/CollectionService.Tests/GetUserCollections_Should.cs
@@ -1,121 +1,192 @@
-﻿// using System;
-// using System.Collections.Generic;
-// using System.Diagnostics;
-// using System.Linq;
-// using System.Text;
-// using Microsoft.VisualStudio.TestTools.UnitTesting;
-// using Moq;
-// using RTWTR.Data.Access.Contracts;
-// using RTWTR.Data.Models;
-// using RTWTR.DTO;
-// using RTWTR.Infrastructure.Mapping.Provider;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using RTWTR.Data.Access.Contracts;
+using RTWTR.Data.Models;
+using RTWTR.DTO;
+using RTWTR.Infrastructure.Exceptions;
+using RTWTR.Infrastructure.Mapping.Provider;
 
-// namespace RTWTR.Tests.RTWTR.Service.Data.Tests.CollectionService.Tests
-// {
-//     [TestClass]
-//     public class GetUserCollections_Should
-//     {
-//         private Mock<ISaver> saverStub;
-//         private Mock<IMappingProvider> mapperStub;
-//         private Mock<IRepository<Tweet>> tweetRepositoryStub;
-//         private Mock<IRepository<Collection>> collectionRepositoryStub;
-//         private Mock<IRepository<CollectionTweet>> collectionTweetsRepositoryStub;
+namespace RTWTR.Tests.RTWTR.Service.Data.Tests.CollectionService.Tests
+{
+    [TestClass]
+    public class GetUserCollections_Should
+    {
+        private Mock<ISaver> saverStub;
+        private Mock<IMappingProvider> mapperStub;
+        private Mock<IRepository<Tweet>> tweetRepositoryStub;
+        private Mock<IRepository<Collection>> collectionRepositoryStub;
+        private Mock<IRepository<CollectionTweet>> collectionTweetsRepositoryStub;
 
-//         [TestInitialize]
-//         public void TestInitialize()
-//         {
-//             // Arrange
-//             this.saverStub = new Mock<ISaver>();
-//             this.mapperStub = new Mock<IMappingProvider>();
-//             this.tweetRepositoryStub = new Mock<IRepository<Tweet>>();
-//             this.collectionRepositoryStub = new Mock<IRepository<Collection>>();
-//             this.collectionTweetsRepositoryStub = new Mock<IRepository<CollectionTweet>>();
-//         }
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            // Arrange
+            this.saverStub = new Mock<ISaver>();
+            this.mapperStub = new Mock<IMappingProvider>();
+            this.tweetRepositoryStub = new Mock<IRepository<Tweet>>();
+            this.collectionRepositoryStub = new Mock<IRepository<Collection>>();
+            this.collectionTweetsRepositoryStub = new Mock<IRepository<CollectionTweet>>();
+        }
 
-//         [TestMethod]
-//         public void ReturnNull_When_InvokedWithNullUserId()
-//         {
-//             // Arrange
-//             var collectionService = new global::RTWTR.Service.Data.CollectionService(
-//                 this.saverStub.Object,
-//                 this.mapperStub.Object,
-//                 this.collectionRepositoryStub.Object,
-//                 this.tweetRepositoryStub.Object,
-//                 this.collectionTweetsRepositoryStub.Object
-//             );
+        [TestMethod]
+        public void ReturnNull_When_InvokedWithNullUserId()
+        {
+            // Arrange
+            var collectionService = new global::RTWTR.Service.Data.CollectionService(
+                this.saverStub.Object,
+                this.mapperStub.Object,
+                this.collectionRepositoryStub.Object,
+                this.tweetRepositoryStub.Object,
+                this.collectionTweetsRepositoryStub.Object
+            );
 
-//             // Act & Assert
-//             Assert.IsNull(collectionService.GetUserCollections(null));
-//         }
+            // Act & Assert
+            Assert.ThrowsException<InvalidUserIdException>(() =>
+            {
+                collectionService.GetUserCollections(null);
+            });
+        }
 
-//         [TestMethod]
-//         public void ReturnZeroCollections_When_UserHasNoCollections()
-//         {
-//             // Arrange
-//             this.collectionRepositoryStub
-//                 .Setup(x => x.All)
-//                 .Returns(
-//                     new List<Collection>()
-//                     {
-//                         new Collection() { Name = "testCollection", UserId = "notThisOne" }
-//                     }.AsQueryable()
-//                 );
+        [TestMethod]
+        public void ReturnZeroCollections_When_UserHasNoCollections()
+        {
+            // Arrange
+            this.mapperStub
+                .Setup(x => x.ProjectTo<CollectionDTO>(It.IsAny<IQueryable<object>>()))
+                .Returns(new List<CollectionDTO>().AsQueryable());
 
-//             this.mapperStub
-//                 .Setup(x => x.ProjectTo<CollectionDTO>(It.IsAny<IQueryable<object>>()))
-//                 .Returns(new List<CollectionDTO>().AsQueryable());
+            var collectionService = new global::RTWTR.Service.Data.CollectionService(
+                this.saverStub.Object,
+                this.mapperStub.Object,
+                this.collectionRepositoryStub.Object,
+                this.tweetRepositoryStub.Object,
+                this.collectionTweetsRepositoryStub.Object
+            );
 
-//             var collectionService = new global::RTWTR.Service.Data.CollectionService(
-//                 this.saverStub.Object,
-//                 this.mapperStub.Object,
-//                 this.collectionRepositoryStub.Object,
-//                 this.tweetRepositoryStub.Object,
-//                 this.collectionTweetsRepositoryStub.Object
-//             );
+            // Act & Assert
+            Assert.AreEqual(
+                0,
+                collectionService.GetUserCollections("userId").Count()
+            );
+        }
 
-//             // Act & Assert
-//             Assert.AreEqual(
-//                 0,
-//                 collectionService.GetUserCollections("1234567890").Count()
-//             );
-//         }
+        [TestMethod]
+        public void Call_CollectionRepository_All_Once()
+        {
+            // Arrange
+            this.collectionRepositoryStub
+                .Setup(x => x.All)
+                .Returns(new List<Collection>()
+                {
+                    new Collection() { UserId = "userId"}
+                }.AsQueryable())
+                .Verifiable();
+            
+            var collectionService = new global::RTWTR.Service.Data.CollectionService(
+                this.saverStub.Object,
+                this.mapperStub.Object,
+                this.collectionRepositoryStub.Object,
+                this.tweetRepositoryStub.Object,
+                this.collectionTweetsRepositoryStub.Object
+            );
 
-//         [TestMethod]
-//         public void ReturnCorrectCollections_When_UserIsValid()
-//         {
-//             // Arrange
-//             this.collectionRepositoryStub
-//                 .Setup(x => x.All)
-//                 .Returns(
-//                     new List<Collection>()
-//                     {
-//                         new Collection() { Name = "nope", UserId = "notThisOne" },
-//                         new Collection() { Name = "test1", UserId = "1234567890" },
-//                         new Collection() { Name = "test2", UserId = "1234567890" }
-//                     }.AsQueryable());
+            // Act
+            collectionService.GetUserCollections("userId");
 
-//             this.mapperStub
-//                 .Setup(x => x.ProjectTo<CollectionDTO>(It.IsAny<IQueryable<object>>()))
-//                 .Returns(
-//                     new List<CollectionDTO>()
-//                     {
-//                         new CollectionDTO() { Name = "test1" },
-//                         new CollectionDTO() { Name = "test2" }
-//                     }.AsQueryable());
+            // Assert
+            this.collectionRepositoryStub.Verify(
+                x => x.All,
+                Times.Once
+            );
+        }
 
-//             var collectionService = new global::RTWTR.Service.Data.CollectionService(
-//                 this.saverStub.Object,
-//                 this.mapperStub.Object,
-//                 this.collectionRepositoryStub.Object,
-//                 this.tweetRepositoryStub.Object,
-//                 this.collectionTweetsRepositoryStub.Object
-//             );
+        [TestMethod]
+        public void Call_Mapper_ProjectTo_Once()
+        {
+            // Arrange
+            this.collectionRepositoryStub
+                .Setup(x => x.All)
+                .Returns(new List<Collection>()
+                {
+                    new Collection() { UserId = "userId"}
+                }.AsQueryable());
+            
+            this.mapperStub
+                .Setup(x => x.ProjectTo<CollectionDTO>(It.IsAny<IQueryable<Collection>>()))
+                .Verifiable();
+            
+            var collectionService = new global::RTWTR.Service.Data.CollectionService(
+                this.saverStub.Object,
+                this.mapperStub.Object,
+                this.collectionRepositoryStub.Object,
+                this.tweetRepositoryStub.Object,
+                this.collectionTweetsRepositoryStub.Object
+            );
 
-//             // Act & Assert
-//             Assert.AreEqual(
-//                 2,
-//                 collectionService.GetUserCollections("1234567890").Count()
-//             );
-//         }
-//     }
-// }
+            // Act
+            collectionService.GetUserCollections("userId");
+
+            // Assert
+            this.mapperStub.Verify(
+                x => x.ProjectTo<CollectionDTO>(It.IsAny<IQueryable<Collection>>()),
+                Times.Once
+            );
+        }
+
+        [TestMethod]
+        public void ReturnCorrectCollections_When_UserIsValid()
+        {
+            // Arrange
+            this.collectionRepositoryStub
+                .Setup(x => x.All)
+                .Returns(
+                    new List<Collection>()
+                    {
+                        new Collection() { Name = "nope", UserId = "notThisOne" },
+                        new Collection() { Name = "test1", UserId = "userId" },
+                        new Collection() { Name = "test2", UserId = "userId" }
+                    }.AsQueryable());
+
+            this.mapperStub
+                .Setup(x => x.ProjectTo<CollectionDTO>(It.IsAny<IQueryable<object>>()))
+                .Returns(
+                    new List<CollectionDTO>()
+                    {
+                        new CollectionDTO() { Name = "test1" },
+                        new CollectionDTO() { Name = "test2" }
+                    }.AsQueryable());
+
+            var collectionService = new global::RTWTR.Service.Data.CollectionService(
+                this.saverStub.Object,
+                this.mapperStub.Object,
+                this.collectionRepositoryStub.Object,
+                this.tweetRepositoryStub.Object,
+                this.collectionTweetsRepositoryStub.Object
+            );
+
+            // Act
+            var collections = collectionService.GetUserCollections("userId").ToList();
+
+            // Assert
+            Assert.AreEqual(
+                2,
+                collections.Count()
+            );
+
+            Assert.AreEqual(
+                "test1",
+                collections[0].Name
+            );
+
+            Assert.AreEqual(
+                "test2",
+                collections[1].Name
+            );
+        }
+    }
+}
