472c9354387b942536e1b96663f79ecc453edfae
Nikolay Nikolov <nikolaytsnikolov@gmail.com>
2018-04-29 14:40:46 +0300

    Mapping DTOs to JSON properties

--- RTWTR/RTWTR.DTO/TweetDTO.cs
+++ RTWTR/RTWTR.DTO/TweetDTO.cs
@@ -1,15 +1,19 @@
-﻿using RTWTR.Data.Models;
+﻿using Newtonsoft.Json;
 
 namespace RTWTR.DTO
 {
     public class TweetDto 
     {
+        [JsonProperty("id")]
         public string Id { get; set; }
-        
+
+        [JsonProperty("text")]
         public string Text { get; set; }
 
+        [JsonProperty("user")]
         public TwitterUserDto User { get; set; }
 
+        [JsonProperty("created_at")]
         public string CreatedAt { get; set; }
     }
 }
--- RTWTR/RTWTR.DTO/TwitterUserDto.cs
+++ RTWTR/RTWTR.DTO/TwitterUserDto.cs
@@ -1,15 +1,22 @@
-﻿namespace RTWTR.DTO
+﻿using Newtonsoft.Json;
+
+namespace RTWTR.DTO
 {
     public class TwitterUserDto
     {
+        [JsonProperty("id_str")]
         public string Id { get; set; }
 
+        [JsonProperty("screen_name")]
         public string ScreenName { get; set; }
 
+        [JsonProperty("name")]
         public string Name { get; set; }
 
+        [JsonProperty("description")]
         public string Description { get; set; }
 
+        [JsonProperty("profile_image_url")]
         public string ProfileImageUrl { get; set; }
     }
 }
