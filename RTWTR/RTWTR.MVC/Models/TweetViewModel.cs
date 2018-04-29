c341c0c441a975e6b77cb1e1a260aebc581e0607
Nikolay Nikolov <nikolaytsnikolov@gmail.com>
2018-04-29 14:54:42 +0300

    Adding TwitterUser,Tweet ViewModels

---
+++ RTWTR/RTWTR.MVC/Models/TweetViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RTWTR.MVC.Models
+{
+    public class TweetViewModel
+    {
+        public string Id { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public string Text { get; set; }
+
+        public string TwitterUserId { get; set; }
+
+        public string TwitterUserScreenName { get; set; }
+
+        
+    }
+}
---
+++ RTWTR/RTWTR.MVC/Models/TwitterUserViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace RTWTR.MVC.Models
+{
+    public class TwitterUserViewModel
+    {
+        public string Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string ScreenName { get; set; }
+
+        public string Description { get; set; }
+
+        public string ProfileImageUrl { get; set; }
+
+    }
+}
