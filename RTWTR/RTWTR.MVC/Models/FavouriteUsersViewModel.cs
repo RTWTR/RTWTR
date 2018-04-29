4badb2991f6f0087fb261a5b81a00d7949e17d17
Nikolay Nikolov <nikolaytsnikolov@gmail.com>
2018-04-30 00:54:53 +0300

    Adding FavouriteUsersViewModel

---
+++ RTWTR/RTWTR.MVC/Models/FavouriteUsersViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RTWTR.MVC.Models
+{
+    public class FavouriteUsersViewModel
+    {
+        public ICollection<TwitterUserViewModel> Tweets { get; set; }
+    }
+}
