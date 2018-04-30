8e372607e094371eebb83e6f37580827f06d2dfc
Nikolay Nikolov <nikolaytsnikolov@gmail.com>
2018-04-30 20:22:49 +0300

    Adding footer planel

--- RTWTR/RTWTR.MVC/Controllers/AccountController.cs
+++ RTWTR/RTWTR.MVC/Controllers/AccountController.cs
@@ -257,7 +257,7 @@ namespace RTWTR.MVC.Controllers
         {
             await _signInManager.SignOutAsync();
             _logger.LogInformation("User logged out.");
-            return RedirectToAction(nameof(HomeController.Index), "Home");
+            return RedirectToAction("Login", "Account");
         }
 
         [HttpPost]
