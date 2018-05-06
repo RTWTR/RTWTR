8e54a93ef5a7d03e7a94ae67d061f70732b3aa3d
IovoslavIovchev <iovoslav.i@gmail.com>
2018-05-06 11:57:44 +0300

    Made more minor changes to the Layout

--- RTWTR/RTWTR.MVC/Controllers/UsersController.cs
+++ RTWTR/RTWTR.MVC/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -156,6 +157,8 @@ namespace RTWTR.MVC.Controllers
             {
                 TwitterUsers = this.mapper.MapTo<List<TwitterUserViewModel>>(favourites)
             };
+
+            model.TwitterUsers.Select(x => { x.IsFavourite = true; return x; }).ToList();
 
             return View(model);
         }
